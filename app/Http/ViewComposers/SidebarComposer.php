868b5315deb6d2e7fea248d341929d5e2f03c285
vladimir3293 <vladimir3293@gmail.com>
2017-11-27 22:56:37 +0200

    work (at home) todo SidebarComposer

--- app/Http/ViewComposers/SidebarComposer.php
+++ app/Http/ViewComposers/SidebarComposer.php
@@ -5,6 +5,7 @@ namespace Deadzombies\Http\ViewComposers;
 
 use Deadzombies\Model\Category;
 //use Illuminate\Support\Facades\Request;
+use Deadzombies\Model\Game;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -13,11 +14,13 @@ class SidebarComposer
     public $category;
 
     public $request;
+    public $game;
 
-    public function __construct(Category $category, Request $request)
+    public function __construct(Category $category, Request $request, Game $game)
     {
         $this->category = $category;
         $this->request = $request;
+        $this->game = $game;
     }
 
     public function compose(View $view)
@@ -25,9 +28,18 @@ class SidebarComposer
 //dd($this->request->path());
 
         if ($this->request->is('category/*')){
+            //$this->game->
+
+
             echo '1221';
         }
         if($this->request->is('/')){
+            $topGames = $this->game->orderBy('game_like','desc')->take(4)->get();
+            $topGames->each(function($value) {
+$cat_url = $value->category()->where('')
+               $value->url = route('getGame',['category'=>$cat_url,'game'=>$value->game_url])
+
+            });
             echo 'koren';
         }
             $categories = $this->category::orderBy('cat_order')->get();
