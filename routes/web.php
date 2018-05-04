ec876ea1a76bc81596613429679a0159160a0785
Vladimir Bondarenko <vladimir3293@gmail.com>
2018-05-04 14:07:57 +0300

    dev at work (wtf)

--- routes/web.php
+++ routes/web.php
@@ -100,3 +100,6 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 Auth::routes();
+
+
+Route::get('/home', 'HomeController@index')->name('home');
