9b77b0e85e5f9b2d5be20969f5def614ac4fbf90
Vladimir Bondarenko <vladimir3293@gmail.com>
2018-09-03 17:16:45 +0300

    dev at work (wtf)

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,6 +5,7 @@ namespace Deadzombies\Http\Controllers;
 
 use Deadzombies\Model\Game;
 use Deadzombies\Model\Page;
+use Illuminate\Database\Eloquent\Collection;
 
 class IndexController extends Controller
 {
@@ -13,21 +14,31 @@ class IndexController extends Controller
         //TODO transfer to model
         //TODO refactor img
         //TODO googleof googleoff
-        //dd($test);
-        //dd($test->update(['game_show'=>1]));
-
-        //$pageIndex = $page->where('name', 'index')->get()->first();
-
-        $popularGames = $game->where('game_show', true)->orderBy('game_like')->simplePaginate(5);
-        //dd($games);
+        $popularGames = $game->where('game_show', true)->orderBy('game_played')->get();
         $popularGames->each(function ($games) {
             $games->url = route('getGame', $games->game_url, false);
             $games->img = file_exists(public_path() . '/img/' . $games->game_url . '.jpg') ?
                 '/img/' . $games->game_url . '.jpg' :
                 '/img/site/empty.jpg';
         });
-        $newGames = $game->where('game_show', true)->simplePaginate(5);
-        return view('index', ['popularGames' => $popularGames,
-            'newGames' => $newGames]);
+        $newGames = $game->where('game_show', true)->orderBy('id','desc')->get();
+        $newGames->each(function ($games) {
+            $games->url = route('getGame', $games->game_url, false);
+            $games->img = file_exists(public_path() . '/img/' . $games->game_url . '.jpg') ?
+                '/img/' . $games->game_url . '.jpg' :
+                '/img/site/empty.jpg';
+        });
+        $bestGames = $game->where('game_show', true)->orderBy('game_like')->get();
+        $bestGames->each(function ($games) {
+            $games->url = route('getGame', $games->game_url, false);
+            $games->img = file_exists(public_path() . '/img/' . $games->game_url . '.jpg') ?
+                '/img/' . $games->game_url . '.jpg' :
+                '/img/site/empty.jpg';
+        });
+        return view('index', [
+            'popularGames' => $popularGames,
+            'newGames' => $newGames,
+            'bestGames' => $bestGames,
+        ]);
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,24 +8,50 @@
 @section('keywords','Мета кейвердс первой страницы')
 
 @section('right_content')
-    <div class="top-games">
-        <header class="header-article">
-            <h1>Популярные игры</h1>
-        </header>
-        @if($popularGames->isNotEmpty())
-            <div class="game-container">
-                @include('gameCard',['games'=>$popularGames])
+    @if($popularGames->isNotEmpty())
+        <div class="top-games-container">
+            <div class="top-games">
+                <div class="header-article">
+                    <h1>Популярные игры</h1>
+                </div>
+
+                <div class="game-container">
+                    @include('gameCard',['games'=>$popularGames])
+                </div>
+                {{--{{ $popularGames->links('vendor.pagination.simpleIndexPage') }}--}}
             </div>
-            {{ $popularGames->links('vendor.pagination.simpleIndexPage') }}
-        @endif
+        </div>
+    @endif
+    @if($bestGames->isNotEmpty())
+        <div class="best-games-container">
+            <div class="best-games">
+                <div class="header-article">
+                    <h1>Лучшие игры</h1>
+                </div>
 
-    </div>
-    <div class="best-games">
+                <div class="game-container">
+                    @include('gameCard',['games'=>$bestGames])
+                </div>
+                {{--{{ $popularGames->links('vendor.pagination.simpleIndexPage') }}--}}
+            </div>
+            </div>
+        </div>
+    @endif
+    @if($newGames->isNotEmpty())
+        <div class="popular-games-container">
+            <div class="popular-games">
+                <div class="header-article">
+                    <h1>Новые игры</h1>
+                </div>
 
-    </div>
-    <div class="popular-games">
-
-    </div>
+                <div class="game-container">
+                    @include('gameCard',['games'=>$newGames])
+                </div>
+                {{--{{ $popularGames->links('vendor.pagination.simpleIndexPage') }}--}}
+            </div>
+            </div>
+        </div>
+    @endif
     <div class="description">
         <div class="text-block">
             <p>Описание главной страницыОписание главной страницы Описание главной страницы Описание главной страницы
